e038f509072795d87d92c48f967206658ceac874
nike <nike@dominiumdao.finance>
2022-05-03 00:29:28 -0600

    remove old contracts

--- contracts/StickmanSagaNFTStaking.sol
+++ contracts/StickmanSagaNFTStaking.sol
@@ -210,43 +210,6 @@ interface IStickmanERC721 {
     function isApprovedForAll(address _owner, address _operator) external view returns (bool);
 }
 
-<<<<<<< HEAD:contracts/NFTStake.sol
-<<<<<<< HEAD
-// contract DOMERC721 {
-//   mapping(uint => NFTType) public NFTTypes;
-//   uint public nftTypesSize;
-
-//   mapping(uint256 => uint) public TokenTypes;
-  
-//   struct NFTType {
-//     uint cost; // Cost for each NFT (in USD)
-//     string name; // Name for each NFT
-//     uint256 rewardAmount; // Reward Amount for each NFT
-//     uint256 stakingCost; // Cost to stake each NFT
-//     string baseExtension; // Path for nft attributes/image
-//     bool available; // Still for sale
-//   }
-// }
-=======
-contract DOMERC721 {
-  mapping(uint => NFTType) public NFTTypes;
-  uint public nftTypesSize;
-
-  mapping(uint256 => uint) public TokenTypes;
-  
-  struct NFTType {
-    uint cost; // Cost for each NFT (in USD)
-    string name; // Name for each NFT
-    uint256 rewardAmount; // Reward Amount for each NFT
-    uint256 stakingCost; // Cost to stake each NFT
-    string baseExtension; // Path for nft attributes/image
-    bool available; // Still for sale
-  }
-}
->>>>>>> 1431461... copy over files
-
-=======
->>>>>>> 6b1c894... add hardhat and contracts along with tests:contracts/StickmanSagaNFTStaking.sol
 // OpenZeppelin Contracts v4.4.1 (token/ERC721/IERC721Receiver.sol)
 
 /**
@@ -272,77 +235,22 @@ interface IERC721Receiver {
     ) external returns (bytes4);
 }
 
-<<<<<<< HEAD:contracts/NFTStake.sol
-<<<<<<< HEAD
-contract StickmanSagaNFTDepository is Ownable, IERC721Receiver {
-=======
-contract DominiumNFTDepository is Ownable, IERC721Receiver {
->>>>>>> 1431461... copy over files
-=======
 contract StickmanSagaNFTStaking is Ownable, IERC721Receiver {
->>>>>>> 6b1c894... add hardhat and contracts along with tests:contracts/StickmanSagaNFTStaking.sol
   using SafeERC20 for IERC20;
   using SafeMath for uint256;
 
   address public nftContract;
-<<<<<<< HEAD
-  address public feeCoin;
-<<<<<<< HEAD:contracts/NFTStake.sol
-<<<<<<< HEAD
-  address public treasury; 
-=======
-=======
->>>>>>> 0721aba... revise tests and remove stable coin
   address public stixToken;
->>>>>>> 6b1c894... add hardhat and contracts along with tests:contracts/StickmanSagaNFTStaking.sol
 
   bool public locked; // Locks all deposits, claims, and withdrawls
   uint256 public withdrawlFee; //fee in USDC for withdrawing staked NFT
   uint256 public claimLength; // Length of time between claims
-<<<<<<< HEAD
-  uint256 public claimReward; // Reward per Stickman staked
-<<<<<<< HEAD:contracts/NFTStake.sol
-
-  // uint public maxEpochs; // How many times the rewards will accumulate til monthly fee will need to be paid
-
-  // mapping(address => uint256[]) private deposits; // Each address mapped to all the deposited token IDs
-=======
-  address public initialFeeCoin;
-  address public treasury; 
-  address public liquidityPair;
-
-  bool public locked; // Locks all deposits, claims, and withdrawls
-  uint256 public feeLength; // Amount of time til fee expires
-  uint256 public initialFeeLength; // Amout of time til the next fee after initial deposit
-  uint256 public claimLength; // Length of time between claims
-
-  uint256 public feeLiquidityPercent; // Percent of fee going to the liquidity
-
-  uint public maxClaimFee; // Max that the fee can be before it resets on monthly fee pay
-  uint public claimFeeIncrement; // Amount that the fee will increment upon claim
-
-  uint public maxEpochs; // How many times the rewards will accumulate til monthly fee will need to be paid
-
-  mapping(uint => uint256) public monthlyCost; // Each Token Type mapped to a monthly cost
-
-  mapping(address => uint256[]) private deposits; // Each address mapped to all the deposited token IDs
->>>>>>> 1431461... copy over files
-=======
->>>>>>> 6b1c894... add hardhat and contracts along with tests:contracts/StickmanSagaNFTStaking.sol
-=======
   uint256 public claimReward = 20; // Reward per Stickman staked
->>>>>>> 499729d... finish tests
  
   mapping(address => vestedInfo) public inventory; // Each token ID mapped to the info about each one
 
   struct vestedInfo {
-<<<<<<< HEAD:contracts/NFTStake.sol
-    address owner; // Address of the owner
-<<<<<<< HEAD
-    uint256 claimLength; // Current length of time between claims
-=======
     uint256 lastClaimTime; // Current length of time between claims
->>>>>>> 6b1c894... add hardhat and contracts along with tests:contracts/StickmanSagaNFTStaking.sol
     bool locked; // Lock NFT to prevent claiming or withdraw
     uint8[] depositedNFTs; //keep track of all the NFTs deposited
     uint256 rewardAmount; //when number of NFTs changes, update this number
@@ -379,90 +287,19 @@ contract StickmanSagaNFTStaking is Ownable, IERC721Receiver {
 
   constructor(
     address _nftContract, // Stickman Saga NFT contract
-<<<<<<< HEAD
-<<<<<<< HEAD
-    address _feeCoin,
-<<<<<<< HEAD:contracts/NFTStake.sol
-=======
-    uint256 epochs; // How many times claimed
-    uint256 nextTimestamp; // Timestamp of next claim
-    uint256 feeExpiration; // Timestamp of when fee time expires
-    uint256 claimLength; // Current length of time between claims
-    uint claimFee; // Current fee to claim balance (out of 1000)
-    uint maxEpochs; // Current maxEpochs
-    bool locked; // Lock NFT to prevent claiming or withdraw
-  }
-  
-    uint256 private unlocked = 1;
-    modifier lock() {
-        require(unlocked == 1, "Queue Locked");
-        unlocked = 0;
-        _;
-        unlocked = 1;
-    }
-
-  constructor(
-    address _nftContract, // 0xa04a030f4c8b22b11e98e1cbaf280e0ff01fed79
-    address _feeCoin,
-    address _initialFeeCoin,
->>>>>>> 1431461... copy over files
-    address _treasury
-  ) {
-    nftContract = _nftContract;
-    feeCoin = _feeCoin;
-<<<<<<< HEAD
-    treasury = _treasury;
-    claimLength = 1 days;
-=======
-    initialFeeCoin = _initialFeeCoin;
-    treasury = _treasury;
-    feeLength = 30 days;
-    initialFeeLength = 60 days;
-    claimLength = 1 days;
-
-    feeLiquidityPercent = 30;
-
-    maxEpochs = 32;
-
-    maxClaimFee = 200;
-    claimFeeIncrement = 50;
->>>>>>> 1431461... copy over files
-=======
-    address _stixToken
-=======
-    address _feeCoin,  // Stable Coin or can even be ETH -> would need to 
-=======
->>>>>>> 0721aba... revise tests and remove stable coin
     address _stixToken // STIX token contract
->>>>>>> 499729d... finish tests
   ) {
     nftContract = _nftContract;
     claimLength = 1 days;
     stixToken = _stixToken;
-<<<<<<< HEAD
-    withdrawlFee = 20 * 10**IERC20(feeCoin).decimals();
->>>>>>> 6b1c894... add hardhat and contracts along with tests:contracts/StickmanSagaNFTStaking.sol
-=======
     withdrawlFee = 20 * 10**18;
->>>>>>> 0721aba... revise tests and remove stable coin
   }
 
   function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
       return this.onERC721Received.selector;
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD:contracts/NFTStake.sol
-<<<<<<< HEAD
-  //TODO: create a depositAll, change to pull all the NFTs from a wallet to deposit
-  function depositNFTs(uint256[] tokenIds) public reentrancyGuard  checkNFTOwner(tokenIds, msg.sender) {
-=======
-  //TODO: only allow 5 NFTs to be staked at one time
-  function depositNFTs(uint256[] calldata tokenIds) public reentrancyGuard checkNFTOwner(tokenIds, msg.sender) {
->>>>>>> 6b1c894... add hardhat and contracts along with tests:contracts/StickmanSagaNFTStaking.sol
-=======
   function depositNFTs(uint8[] calldata tokenIds) public reentrancyGuard checkNFTOwner(tokenIds, msg.sender) {
->>>>>>> 499729d... finish tests
     require(!locked, "Deposit: All deposits are currently locked.");
     require(tokenIds.length + inventory[msg.sender].depositedNFTs.length >= 2, "Deposit: you must deposit at least 2 NFTs");
     inventory[msg.sender].rewardAmount = calculateRewards(msg.sender);
@@ -487,108 +324,14 @@ contract StickmanSagaNFTStaking is Ownable, IERC721Receiver {
   
   function claim() public reentrancyGuard {
     require(!locked, "Claim: All claims are currently locked.");
-<<<<<<< HEAD:contracts/NFTStake.sol
-    // require(!inventory[tokenID].locked, "Claim: Claim is locked for this token ID.");
-    // require(inventory[tokenID].owner == msg.sender, "Claim: In order to claim you must be the owner.");
-    deposits[msg.sender].length;
-=======
-  function deposit(uint256 tokenID) public lock {
-    require(!locked, "Deposit: All deposits are currently locked.");
-    require(ERC721(nftContract).ownerOf(tokenID) == msg.sender, "Deposit: You are not the owner of this token ID.");
-    require(IERC20(initialFeeCoin).balanceOf(msg.sender) >= getStakingCost(tokenID), "PayFee: You don't have enough for the fee.");
-
-    addDeposit(msg.sender, tokenID);
-
-    ERC721(nftContract).safeTransferFrom(msg.sender, address(this), tokenID);
-
-    IERC20(initialFeeCoin).safeTransferFrom(msg.sender, address(this), getStakingCost(tokenID));
-    IERC20(initialFeeCoin).safeTransfer( liquidityPair, getStakingCost(tokenID).div(100).mul(feeLiquidityPercent)); // Going to the liqudity
-    IERC20(initialFeeCoin).safeTransfer( treasury, getStakingCost(tokenID).div(100).mul(100 - feeLiquidityPercent)); // Going to the treasury
-  }
-
-  function withdraw(uint256 tokenID) public lock {
-    require(!locked, "Withdraw: All withdrawls are currently locked.");
-    require(!inventory[tokenID].locked, "Withdraw: Withdraw is locked for this token ID.");
-    require(inventory[tokenID].owner == msg.sender, "Withdraw: You are not the owner for this token ID.");
-    
-    ERC721(nftContract).safeTransferFrom(address(this), msg.sender, tokenID);
-    deleteDeposit(msg.sender, tokenID);
-  }
-
-  function claimAll() public {
-    require(deposits[msg.sender].length > 0, "ClaimAll: No NFTs available to claim");
-    
-    for ( uint i=0; i < deposits[msg.sender].length; i++ ) {
-      if( block.timestamp >= inventory[deposits[msg.sender][i]].nextTimestamp ){
-        claim(deposits[msg.sender][i]);
-      }
-    }
-  }
-  
-  function payFeeAll() public lock {
-    require(deposits[msg.sender].length > 0, "ClaimAll: No NFTs available to claim");
-    
-    for ( uint i=0; i < deposits[msg.sender].length; i++ ) {
-      if( !isFeePaid( deposits[msg.sender][i] ) ){
-        payFee( deposits[msg.sender][i] );
-      }
-    }
-  }
-  
-  function claim(uint256 tokenID) public lock {
-    require(!locked, "Claim: All claims are currently locked.");
-    require(!inventory[tokenID].locked, "Claim: Claim is locked for this token ID.");
-    require(isFeePaid(tokenID), "Claim: Fee required to be paid for this token ID.");
-    require(inventory[tokenID].owner == msg.sender, "Claim: In order to claim you must be the owner.");
->>>>>>> 1431461... copy over files
-
-    if (block.timestamp >= inventory[tokenID].nextTimestamp) {
-      claimBalance(tokenID);
-    }
-=======
     claimBalance(msg.sender);
->>>>>>> 6b1c894... add hardhat and contracts along with tests:contracts/StickmanSagaNFTStaking.sol
   }
 
-<<<<<<< HEAD
-=======
-  function payFee(uint256 tokenID) public lock {
-    require(inventory[tokenID].owner == msg.sender, "PayFee: You are not the owner.");
-    require(!isFeePaid(tokenID), "PayFee: You still have enough time in your period.");
-    require(IERC20(feeCoin).balanceOf(msg.sender) >= monthlyCost[getTokenType(tokenID)], "PayFee: You don't have enough for the fee.");
-    
-    IERC20(feeCoin).safeTransferFrom(msg.sender, treasury, monthlyCost[getTokenType(tokenID)]);
-
-    claimBalance(tokenID);
-
-    inventory[tokenID].feeExpiration = block.timestamp + feeLength;
-    inventory[tokenID].nextTimestamp = block.timestamp;
-    inventory[tokenID].claimFee = 0;
-    inventory[tokenID].epochs = 0;
-    inventory[tokenID].maxEpochs = maxEpochs;
-  }
-
->>>>>>> 1431461... copy over files
   function balanceOf(address _address) public view returns (uint) {
     return inventory[_address].depositedNFTs.length;
   }
 
   // Policy Functions
-<<<<<<< HEAD
-=======
-  function setFeeLength(uint256 _feeLength) public onlyManager() {
-    feeLength = _feeLength;
-  }  
-
-  function setMaxEpochs(uint256 _maxEpochs) public onlyManager() {
-    maxEpochs = _maxEpochs;
-  }  
-
-  function setInitialFeeLength(uint256 _feeLength) public onlyManager() {
-    initialFeeLength = _feeLength;
-  }  
-
->>>>>>> 1431461... copy over files
   function setClaimlength(uint256 _claimLength) public onlyManager() {
     claimLength = _claimLength;
   }
@@ -598,33 +341,12 @@ contract StickmanSagaNFTStaking is Ownable, IERC721Receiver {
       payable(_owner).transfer(total);
   }  
 
-<<<<<<< HEAD
   function setWithdrawalFee(uint256 newFee) public onlyManager() {
     withdrawlFee = newFee;
   }
 
   function setClaimReward(uint256 newClaimReward) public onlyManager(){
     claimReward = newClaimReward;
-=======
-  function setClaimFees(uint _feeIncrement, uint _feeMax) public onlyManager() {
-    require( _feeMax <= 1000, "setClaimFees: Max fee is too great.");
-    require( _feeIncrement <= _feeMax, "setClaimFees: Fee increment is too great.");
-    claimFeeIncrement = _feeIncrement;
-    maxClaimFee = _feeMax;
-  }  
-
-  function setMonthlyCost(uint _tokenType, uint256 _amount) public onlyManager() {
-    monthlyCost[_tokenType] = _amount;
-  }  
-
-  function setLiquidityFeePercent(uint256 _fee) public onlyManager() {
-    require( _fee <= 100, "Fee too high" );
-    feeLiquidityPercent = _fee;
-  }
-
-  function setLiquidityPairAddress(address _address) public onlyManager() {
-    liquidityPair = _address;
->>>>>>> 1431461... copy over files
   }
 
   // function setFeeCoin(address feeCoinAddress) public onlyManager(){
@@ -659,75 +381,10 @@ contract StickmanSagaNFTStaking is Ownable, IERC721Receiver {
     locked = !locked;
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD:contracts/NFTStake.sol
-<<<<<<< HEAD
-  enum CONTRACTS { nftContract, feeCoin, treasury }
-=======
-  enum CONTRACTS { nftContract, feeCoin, treasury, initialfeecoin }
->>>>>>> 1431461... copy over files
-=======
-  enum CONTRACTS { nftContract, feeCoin, stixToken }
->>>>>>> 6b1c894... add hardhat and contracts along with tests:contracts/StickmanSagaNFTStaking.sol
-  function setContract(CONTRACTS _contracts, address _address) public onlyManager() {
-    if (_contracts == CONTRACTS.nftContract) { // 0
-      nftContract = _address;
-    } else if (_contracts == CONTRACTS.feeCoin) { // 1
-      feeCoin = _address;
-<<<<<<< HEAD:contracts/NFTStake.sol
-    }else if (_contracts == CONTRACTS.treasury) { // 2
-      treasury = _address;
-<<<<<<< HEAD
-    } 
-    // else if (_contracts == CONTRACTS.initialfeecoin) { // 3
-    //   initialFeeCoin = _address;
-    // }
-=======
-    } else if (_contracts == CONTRACTS.initialfeecoin) { // 3
-      initialFeeCoin = _address;
-    }
->>>>>>> 1431461... copy over files
-  }
-
-  // Internal Functions
-  function addDeposit(address _recipient, uint256 _tokenID) internal {
-    require(inventory[_tokenID].owner == address(0x0), "addDeposit: Token ID already exists.");
-<<<<<<< HEAD
-    inventory[_tokenID].owner = _recipient;
-    inventory[_tokenID].claimLength = claimLength;
-    inventory[_tokenID].locked = false;
-=======
-
-    inventory[_tokenID].owner = _recipient;
-    inventory[_tokenID].nextTimestamp = block.timestamp;
-    inventory[_tokenID].claimLength = claimLength;
-    inventory[_tokenID].epochs = 0;
-    inventory[_tokenID].claimFee = 0;
-    inventory[_tokenID].maxEpochs = 65;
-    inventory[_tokenID].locked = false;
-    inventory[_tokenID].feeExpiration = block.timestamp + initialFeeLength;
->>>>>>> 1431461... copy over files
-
-    deposits[_recipient].push(_tokenID);
-  }
-
-  function deleteDeposit(address _recipient, uint256 _tokenID) internal {
-    delete inventory[_tokenID];
-
-    uint256[] memory list = new uint256[](deposits[_recipient].length-1);
-    uint z=0;
-    for (uint i=0; i < deposits[_recipient].length; i++) {
-      if (deposits[_recipient][i] != _tokenID) {
-        list[z] = deposits[_recipient][i];
-        z++;
-      }
-=======
-=======
   enum CONTRACTS { nftContract, stixToken }
   function setContract(CONTRACTS _contracts, address _address) public onlyManager() {
     if (_contracts == CONTRACTS.nftContract) { // 0
       nftContract = _address;
->>>>>>> 0721aba... revise tests and remove stable coin
     }else if (_contracts == CONTRACTS.stixToken) { // 2
       stixToken = _address;
     } 
@@ -750,7 +407,6 @@ contract StickmanSagaNFTStaking is Ownable, IERC721Receiver {
     }
     else {
       return 0;
->>>>>>> 6b1c894... add hardhat and contracts along with tests:contracts/StickmanSagaNFTStaking.sol
     }
   }
 
@@ -786,31 +442,8 @@ contract StickmanSagaNFTStaking is Ownable, IERC721Receiver {
   }
 
   // Visual Functions
-<<<<<<< HEAD
-<<<<<<< HEAD:contracts/NFTStake.sol
-<<<<<<< HEAD
-  // function isFeePaid(uint256 tokenID) public view returns (bool) {
-  //   return block.timestamp <= inventory[tokenID].feeExpiration;
-  // }
-=======
-  function isFeePaid(uint256 tokenID) public view returns (bool) {
-    return block.timestamp <= inventory[tokenID].feeExpiration;
-  }
->>>>>>> 1431461... copy over files
-
-  function listAll(address _address) public view returns (uint256[] memory) {
-    uint256[] memory list = new uint256[](deposits[_address].length);
-    for (uint i=0; i < deposits[_address].length; i++) {
-      list[i] = deposits[_address][i];
-    }
-    return list;
-=======
-  function claimableAmount(address _recipient) public view returns (uint256) {
-=======
   function getClaimableAmount(address _recipient) public view returns (uint256) {
->>>>>>> 499729d... finish tests
     return calculateRewards(_recipient);
->>>>>>> 6b1c894... add hardhat and contracts along with tests:contracts/StickmanSagaNFTStaking.sol
   }
 
   function getTokenIdsForAddressExternal(address nftOwner) public view returns(uint8[] memory){
@@ -824,49 +457,8 @@ contract StickmanSagaNFTStaking is Ownable, IERC721Receiver {
     }
     return tokenIds;
   }
-<<<<<<< HEAD
-<<<<<<< HEAD:contracts/NFTStake.sol
-  
-<<<<<<< HEAD
-  // function getTokenType(uint256 tokenID) public view returns (uint) {
-  //   return DOMERC721(nftContract).TokenTypes(tokenID);
-  // }
-
-  // function getRewardAmount(uint256 tokenID) public view returns (uint256) {
-  //   uint256 _rewardAmount;
-  //   (, , _rewardAmount, , , ) = DOMERC721(nftContract).NFTTypes(getTokenType(tokenID));
-  //   return _rewardAmount;
-  // }
-
-  // function getStakingCost(uint256 tokenID) public view returns (uint256) {
-  //   uint256 _stakingCost; 
-  //   (, , , _stakingCost, , ) = DOMERC721(nftContract).NFTTypes(getTokenType(tokenID));
-  //   return _stakingCost;
-  // }
-=======
-  function getTokenType(uint256 tokenID) public view returns (uint) {
-    return DOMERC721(nftContract).TokenTypes(tokenID);
-  }
-
-  function getRewardAmount(uint256 tokenID) public view returns (uint256) {
-    uint256 _rewardAmount;
-    (, , _rewardAmount, , , ) = DOMERC721(nftContract).NFTTypes(getTokenType(tokenID));
-    return _rewardAmount;
-  }
-
-  function getStakingCost(uint256 tokenID) public view returns (uint256) {
-    uint256 _stakingCost; 
-    (, , , _stakingCost, , ) = DOMERC721(nftContract).NFTTypes(getTokenType(tokenID));
-    return _stakingCost;
-  }
-
->>>>>>> 1431461... copy over files
-=======
->>>>>>> 6b1c894... add hardhat and contracts along with tests:contracts/StickmanSagaNFTStaking.sol
-=======
 
   function getTokenIdsForAddress(address addr) public view returns(uint8[] memory){
     return inventory[addr].depositedNFTs;
   }
->>>>>>> 499729d... finish tests
 }
