e68376f7c5ea2e6bf3164da114ca6a1c17751233
nike <nike@dominiumdao.finance>
2022-05-03 00:29:28 -0600

    start of staking NFT

--- contracts/NFTStake.sol
+++ contracts/NFTStake.sol
@@ -209,21 +209,21 @@ interface ERC721 {
     function isApprovedForAll(address _owner, address _operator) external view returns (bool);
 }
 
-contract DOMERC721 {
-  mapping(uint => NFTType) public NFTTypes;
-  uint public nftTypesSize;
+// contract DOMERC721 {
+//   mapping(uint => NFTType) public NFTTypes;
+//   uint public nftTypesSize;
 
-  mapping(uint256 => uint) public TokenTypes;
+//   mapping(uint256 => uint) public TokenTypes;
   
-  struct NFTType {
-    uint cost; // Cost for each NFT (in USD)
-    string name; // Name for each NFT
-    uint256 rewardAmount; // Reward Amount for each NFT
-    uint256 stakingCost; // Cost to stake each NFT
-    string baseExtension; // Path for nft attributes/image
-    bool available; // Still for sale
-  }
-}
+//   struct NFTType {
+//     uint cost; // Cost for each NFT (in USD)
+//     string name; // Name for each NFT
+//     uint256 rewardAmount; // Reward Amount for each NFT
+//     uint256 stakingCost; // Cost to stake each NFT
+//     string baseExtension; // Path for nft attributes/image
+//     bool available; // Still for sale
+//   }
+// }
 
 // OpenZeppelin Contracts v4.4.1 (token/ERC721/IERC721Receiver.sol)
 
@@ -250,147 +250,98 @@ interface IERC721Receiver {
     ) external returns (bytes4);
 }
 
-contract DominiumNFTDepository is Ownable, IERC721Receiver {
+contract StickmanSagaNFTDepository is Ownable, IERC721Receiver {
   using SafeERC20 for IERC20;
   using SafeMath for uint256;
 
   address public nftContract;
   address public feeCoin;
-  address public initialFeeCoin;
   address public treasury; 
-  address public liquidityPair;
 
   bool public locked; // Locks all deposits, claims, and withdrawls
-  uint256 public feeLength; // Amount of time til fee expires
-  uint256 public initialFeeLength; // Amout of time til the next fee after initial deposit
+  uint256 public withdrawlFee = 20; //fee in USDC for withdrawing staked NFT
   uint256 public claimLength; // Length of time between claims
+  uint256 public claimReward; // Reward per Stickman staked
 
-  uint256 public feeLiquidityPercent; // Percent of fee going to the liquidity
+  // uint public maxEpochs; // How many times the rewards will accumulate til monthly fee will need to be paid
 
-  uint public maxClaimFee; // Max that the fee can be before it resets on monthly fee pay
-  uint public claimFeeIncrement; // Amount that the fee will increment upon claim
-
-  uint public maxEpochs; // How many times the rewards will accumulate til monthly fee will need to be paid
-
-  mapping(uint => uint256) public monthlyCost; // Each Token Type mapped to a monthly cost
-
-  mapping(address => uint256[]) private deposits; // Each address mapped to all the deposited token IDs
+  // mapping(address => uint256[]) private deposits; // Each address mapped to all the deposited token IDs
  
   mapping(uint256 => vestedInfo) public inventory; // Each token ID mapped to the info about each one
 
   struct vestedInfo {
     address owner; // Address of the owner
-    uint256 epochs; // How many times claimed
-    uint256 nextTimestamp; // Timestamp of next claim
-    uint256 feeExpiration; // Timestamp of when fee time expires
     uint256 claimLength; // Current length of time between claims
-    uint claimFee; // Current fee to claim balance (out of 1000)
-    uint maxEpochs; // Current maxEpochs
     bool locked; // Lock NFT to prevent claiming or withdraw
+    uint numNFTS; //track number of NFTs staked
   }
   
-    uint256 private unlocked = 1;
-    modifier lock() {
-        require(unlocked == 1, "Queue Locked");
-        unlocked = 0;
-        _;
-        unlocked = 1;
+  /** reentrancy */
+  uint256 private guard = 1;
+  modifier reentrancyGuard() {
+      require (guard == 1, "reentrancy failure.");
+      guard = 2;
+      _;
+      guard = 1;
+  }
+
+  // modifiers functions
+  modifier checkNFTOwner(uint256[] tokenIds, address owner){
+    for (uint256 index = 0; index < tokenIds.length; index++) {
+      require(owner == ERC721(nftContract).ownerOf(tokenIds[index]));
     }
+    _;
+  }
 
   constructor(
-    address _nftContract, // 0xa04a030f4c8b22b11e98e1cbaf280e0ff01fed79
+    address _nftContract, // Stickman Saga NFT contract
     address _feeCoin,
-    address _initialFeeCoin,
     address _treasury
   ) {
     nftContract = _nftContract;
     feeCoin = _feeCoin;
-    initialFeeCoin = _initialFeeCoin;
     treasury = _treasury;
-    feeLength = 30 days;
-    initialFeeLength = 60 days;
     claimLength = 1 days;
-
-    feeLiquidityPercent = 30;
-
-    maxEpochs = 32;
-
-    maxClaimFee = 200;
-    claimFeeIncrement = 50;
   }
 
   function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
       return this.onERC721Received.selector;
   }
 
-  function deposit(uint256 tokenID) public lock {
+  //TODO: create a depositAll, change to pull all the NFTs from a wallet to deposit
+  function depositNFTs(uint256[] tokenIds) public reentrancyGuard  checkNFTOwner(tokenIds, msg.sender) {
     require(!locked, "Deposit: All deposits are currently locked.");
-    require(ERC721(nftContract).ownerOf(tokenID) == msg.sender, "Deposit: You are not the owner of this token ID.");
-    require(IERC20(initialFeeCoin).balanceOf(msg.sender) >= getStakingCost(tokenID), "PayFee: You don't have enough for the fee.");
+    require(ERC721(nftContract).balanceOf(msg.sender) > 2, "Deposit: you must own at least 2 NFTs");
 
-    addDeposit(msg.sender, tokenID);
-
-    ERC721(nftContract).safeTransferFrom(msg.sender, address(this), tokenID);
-
-    IERC20(initialFeeCoin).safeTransferFrom(msg.sender, address(this), getStakingCost(tokenID));
-    IERC20(initialFeeCoin).safeTransfer( liquidityPair, getStakingCost(tokenID).div(100).mul(feeLiquidityPercent)); // Going to the liqudity
-    IERC20(initialFeeCoin).safeTransfer( treasury, getStakingCost(tokenID).div(100).mul(100 - feeLiquidityPercent)); // Going to the treasury
+    for (uint256 index = 0; index < tokenIds.length; index++) {
+      require(ERC721(nftContract).ownerOf(tokenIds[index]) == msg.sender, "Deposit: You are not the owner of this token ID.");
+      addDeposit(msg.sender, tokenIds[index]);
+      ERC721(nftContract).safeTransferFrom(msg.sender, address(this), tokenIds[index]);
+    }
   }
 
-  function withdraw(uint256 tokenID) public lock {
+  function withdraw(uint256[] tokenIds) public reentrancyGuard {
     require(!locked, "Withdraw: All withdrawls are currently locked.");
     require(!inventory[tokenID].locked, "Withdraw: Withdraw is locked for this token ID.");
     require(inventory[tokenID].owner == msg.sender, "Withdraw: You are not the owner for this token ID.");
-    
+    require(IERC20(feeCoin).balanceOf(msg.sender) >= withdrawlFee, "Withdrawl Fee: You don't have enough for the fee.");
+    IERC20(feeCoin).safeTransferFrom(msg.sender, address(this), withdrawlFee); //TODO: change staking cost to something
     ERC721(nftContract).safeTransferFrom(address(this), msg.sender, tokenID);
     deleteDeposit(msg.sender, tokenID);
   }
-
-  function claimAll() public {
-    require(deposits[msg.sender].length > 0, "ClaimAll: No NFTs available to claim");
-    
-    for ( uint i=0; i < deposits[msg.sender].length; i++ ) {
-      if( block.timestamp >= inventory[deposits[msg.sender][i]].nextTimestamp ){
-        claim(deposits[msg.sender][i]);
-      }
-    }
-  }
   
-  function payFeeAll() public lock {
-    require(deposits[msg.sender].length > 0, "ClaimAll: No NFTs available to claim");
-    
-    for ( uint i=0; i < deposits[msg.sender].length; i++ ) {
-      if( !isFeePaid( deposits[msg.sender][i] ) ){
-        payFee( deposits[msg.sender][i] );
-      }
-    }
-  }
-  
-  function claim(uint256 tokenID) public lock {
+  //TODO: figure out how claiming works, they will have to be staked at a certain time and each day it will check if they were staked?
+  // with multiple it makes it tricky because you cant just check how many they have staked, because there could be overlap somehow?
+  //need to find a protocol/contract that streams tokens
+  function claim() public reentrancyGuard {
     require(!locked, "Claim: All claims are currently locked.");
-    require(!inventory[tokenID].locked, "Claim: Claim is locked for this token ID.");
-    require(isFeePaid(tokenID), "Claim: Fee required to be paid for this token ID.");
-    require(inventory[tokenID].owner == msg.sender, "Claim: In order to claim you must be the owner.");
+    // require(!inventory[tokenID].locked, "Claim: Claim is locked for this token ID.");
+    // require(inventory[tokenID].owner == msg.sender, "Claim: In order to claim you must be the owner.");
+    deposits[msg.sender].length;
 
     if (block.timestamp >= inventory[tokenID].nextTimestamp) {
       claimBalance(tokenID);
     }
-  }
-
-  function payFee(uint256 tokenID) public lock {
-    require(inventory[tokenID].owner == msg.sender, "PayFee: You are not the owner.");
-    require(!isFeePaid(tokenID), "PayFee: You still have enough time in your period.");
-    require(IERC20(feeCoin).balanceOf(msg.sender) >= monthlyCost[getTokenType(tokenID)], "PayFee: You don't have enough for the fee.");
-    
-    IERC20(feeCoin).safeTransferFrom(msg.sender, treasury, monthlyCost[getTokenType(tokenID)]);
-
-    claimBalance(tokenID);
-
-    inventory[tokenID].feeExpiration = block.timestamp + feeLength;
-    inventory[tokenID].nextTimestamp = block.timestamp;
-    inventory[tokenID].claimFee = 0;
-    inventory[tokenID].epochs = 0;
-    inventory[tokenID].maxEpochs = maxEpochs;
   }
 
   function balanceOf(address _address) public view returns (uint) {
@@ -398,40 +349,16 @@ contract DominiumNFTDepository is Ownable, IERC721Receiver {
   }
 
   // Policy Functions
-  function setFeeLength(uint256 _feeLength) public onlyManager() {
-    feeLength = _feeLength;
-  }  
-
-  function setMaxEpochs(uint256 _maxEpochs) public onlyManager() {
-    maxEpochs = _maxEpochs;
-  }  
-
-  function setInitialFeeLength(uint256 _feeLength) public onlyManager() {
-    initialFeeLength = _feeLength;
-  }  
-
   function setClaimlength(uint256 _claimLength) public onlyManager() {
     claimLength = _claimLength;
   }  
 
-  function setClaimFees(uint _feeIncrement, uint _feeMax) public onlyManager() {
-    require( _feeMax <= 1000, "setClaimFees: Max fee is too great.");
-    require( _feeIncrement <= _feeMax, "setClaimFees: Fee increment is too great.");
-    claimFeeIncrement = _feeIncrement;
-    maxClaimFee = _feeMax;
-  }  
-
-  function setMonthlyCost(uint _tokenType, uint256 _amount) public onlyManager() {
-    monthlyCost[_tokenType] = _amount;
-  }  
-
-  function setLiquidityFeePercent(uint256 _fee) public onlyManager() {
-    require( _fee <= 100, "Fee too high" );
-    feeLiquidityPercent = _fee;
+  function setWithdrawalFee(uint256 newFee) public onlyManager() {
+    withdrawlFee = newFee;
   }
 
-  function setLiquidityPairAddress(address _address) public onlyManager() {
-    liquidityPair = _address;
+  function setClaimReward(uint256 newClaimReward) public onlyManager(){
+    claimReward = newClaimReward;
   }
 
   function managerSafeNFTWithdrawal(uint256 tokenID, address recipient) public onlyManager() {
@@ -456,7 +383,7 @@ contract DominiumNFTDepository is Ownable, IERC721Receiver {
     locked = !locked;
   }
 
-  enum CONTRACTS { nftContract, feeCoin, treasury, initialfeecoin }
+  enum CONTRACTS { nftContract, feeCoin, treasury }
   function setContract(CONTRACTS _contracts, address _address) public onlyManager() {
     if (_contracts == CONTRACTS.nftContract) { // 0
       nftContract = _address;
@@ -464,23 +391,18 @@ contract DominiumNFTDepository is Ownable, IERC721Receiver {
       feeCoin = _address;
     }else if (_contracts == CONTRACTS.treasury) { // 2
       treasury = _address;
-    } else if (_contracts == CONTRACTS.initialfeecoin) { // 3
-      initialFeeCoin = _address;
-    }
+    } 
+    // else if (_contracts == CONTRACTS.initialfeecoin) { // 3
+    //   initialFeeCoin = _address;
+    // }
   }
 
   // Internal Functions
   function addDeposit(address _recipient, uint256 _tokenID) internal {
     require(inventory[_tokenID].owner == address(0x0), "addDeposit: Token ID already exists.");
-
     inventory[_tokenID].owner = _recipient;
-    inventory[_tokenID].nextTimestamp = block.timestamp;
     inventory[_tokenID].claimLength = claimLength;
-    inventory[_tokenID].epochs = 0;
-    inventory[_tokenID].claimFee = 0;
-    inventory[_tokenID].maxEpochs = 65;
     inventory[_tokenID].locked = false;
-    inventory[_tokenID].feeExpiration = block.timestamp + initialFeeLength;
 
     deposits[_recipient].push(_tokenID);
   }
@@ -529,9 +451,9 @@ contract DominiumNFTDepository is Ownable, IERC721Receiver {
   }
 
   // Visual Functions
-  function isFeePaid(uint256 tokenID) public view returns (bool) {
-    return block.timestamp <= inventory[tokenID].feeExpiration;
-  }
+  // function isFeePaid(uint256 tokenID) public view returns (bool) {
+  //   return block.timestamp <= inventory[tokenID].feeExpiration;
+  // }
 
   function listAll(address _address) public view returns (uint256[] memory) {
     uint256[] memory list = new uint256[](deposits[_address].length);
@@ -551,20 +473,19 @@ contract DominiumNFTDepository is Ownable, IERC721Receiver {
     return toMint;
   }
   
-  function getTokenType(uint256 tokenID) public view returns (uint) {
-    return DOMERC721(nftContract).TokenTypes(tokenID);
-  }
+  // function getTokenType(uint256 tokenID) public view returns (uint) {
+  //   return DOMERC721(nftContract).TokenTypes(tokenID);
+  // }
 
-  function getRewardAmount(uint256 tokenID) public view returns (uint256) {
-    uint256 _rewardAmount;
-    (, , _rewardAmount, , , ) = DOMERC721(nftContract).NFTTypes(getTokenType(tokenID));
-    return _rewardAmount;
-  }
+  // function getRewardAmount(uint256 tokenID) public view returns (uint256) {
+  //   uint256 _rewardAmount;
+  //   (, , _rewardAmount, , , ) = DOMERC721(nftContract).NFTTypes(getTokenType(tokenID));
+  //   return _rewardAmount;
+  // }
 
-  function getStakingCost(uint256 tokenID) public view returns (uint256) {
-    uint256 _stakingCost; 
-    (, , , _stakingCost, , ) = DOMERC721(nftContract).NFTTypes(getTokenType(tokenID));
-    return _stakingCost;
-  }
-
+  // function getStakingCost(uint256 tokenID) public view returns (uint256) {
+  //   uint256 _stakingCost; 
+  //   (, , , _stakingCost, , ) = DOMERC721(nftContract).NFTTypes(getTokenType(tokenID));
+  //   return _stakingCost;
+  // }
 }
--- contracts/Stix.sol
+++ contracts/Stix.sol
@@ -828,34 +828,15 @@ contract Ownable is IOwnable {
     }
 }
 
-contract VaultOwned is Ownable {
-    
-    address internal _vault;
-
-    function setVault( address vault_ ) external onlyOwner() returns ( bool ) {
-        _vault = vault_;
-
-        return true;
-    }
-
-    function vault() public view returns (address) {
-        return _vault;
-    }
-
-    modifier onlyVault() {
-        require( _vault == msg.sender, "VaultOwned: caller is not the Vault" );
-        _;
-    }
-}
-
-contract Dominium is ERC20Permit, VaultOwned {
+contract Stix is ERC20Permit, Ownable {
 
     using SafeMath for uint256;  
     
-    constructor() ERC20("Dominium", "DOM", 9) {
+    constructor() ERC20("Stickman Saga", "STIX", 18) {
+        _mint(100000000000000000000000); //number of wanted tokens * 18
     }
 
-    function mint(address account_, uint256 amount_) public virtual onlyVault() {
+    function mint(address account_, uint256 amount_) public virtual onlyOwner() {
         _mint(account_, amount_);
     }
 
