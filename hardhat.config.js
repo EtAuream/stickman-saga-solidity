561e1d0bb8835f9539493f819294f39bd283e2ca
nike <nike@dominiumdao.finance>
2022-05-08 22:24:17 -0600

    fix to STIX.sol

--- hardhat.config.js
+++ hardhat.config.js
@@ -39,7 +39,16 @@ function mnemonic() {
  */
 module.exports = {
   solidity: "0.8.4",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1000,
+    },
+  },
   networks: {
+    eth-mainnet:{
+
+    }
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
